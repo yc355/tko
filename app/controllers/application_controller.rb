b1e4cc721bbb0e341820231e9d946ea429f1cc37
yc355 <yifan.i.chen@gmail.com>
2016-05-08 16:58:56 -0400

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def bad_route
-    render json: "Don't be scared homie."
+    render json: "Don't be scared homie!"
   end
 end
