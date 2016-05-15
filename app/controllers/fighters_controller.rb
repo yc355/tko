0658cb50c360bb30cdddf100fddc83141fe8337f
yc355 <yifan.i.chen@gmail.com>
2016-05-15 14:42:27 -0400

    use sherdog search instead of google

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,8 +1,8 @@
 class FightersController < ApplicationController
   def fighter_search
     begin
-      result = MMAService.sherdog_link(params[:fighter_search_query])
-      render json: result, status: result.class == Hash ? 200 : 400
+      result = MMAService.fighter_search(params[:fighter_search_query])
+      render json: result
     rescue => error
       render json: error
     end
--- app/services/mma_service.rb
+++ app/services/mma_service.rb
@@ -1,29 +1,35 @@
 class MMAService
-  GOOGLE_URL = "http://www.google.com/search?q="
-  SHERDOG_URL = "http://www.sherdog.com/fighter/"
+  SHERDOG_BASE_URL = "http://www.sherdog.com"
+  SHERDOG_FIGHTFINDER_URL = "/stats/fightfinder?SearchTxt="
 
-  def self.sherdog_link(fighter)
-    return google_search(fighter)
-    # sherdog_links = google_search(fighter).css('.g .r a').select do |link|
-    #   link.to_s.include? SHERDOG_URL
-    # end
-    # if sherdog_links != []
-    #   first_link = sherdog_links.first.attribute('href').to_s
-    #   first_link_endtrim = first_link.partition('&').first
-    #   fir_link_trimmed = first_link_endtrim[first_link_endtrim.index(SHERDOG_URL)..-1]
-    # else
-    #   "Google search returned no Sherdog links."
-    # end
+  def self.sherdog_fighter_link(query)
+    url = URI.parse("#{SHERDOG_BASE_URL}#{SHERDOG_FIGHTFINDER_URL}#{query.strip.gsub('_', '+')}")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    links = Nokogiri::HTML(res.body).css('.fightfinder_result a')
+    if links.present?
+      "#{SHERDOG_BASE_URL}#{Nokogiri::HTML(res.body).css('.fightfinder_result a')[0].attributes['href'].value}"
+    else
+      no_profile_found_message
+    end
   end
 
-  def self.fighter_query(fighter)
-    sherdog_page = sherdog_page(fighter)
-
-    return sherdog_page if sherdog_page == "Google search returned no Sherdog links."
+  def self.fighter_search(query)
+    if sherdog_fighter_link(query) != no_profile_found_message
+      url = URI.parse(sherdog_fighter_link(query))
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      sherdog_page = Nokogiri::HTML(res.body)
+    else
+      return no_profile_found_message
+    end
 
     fighter_hash = fighter_template
-
-    fighter_hash[:link] = sherdog_link(fighter)
+    fighter_hash[:link] = sherdog_fighter_link(query)
     fighter_hash[:img_url] = sherdog_page.css('.bio_fighter [itemprop="image"]').attr('src').to_s
     fighter_hash[:name] = sherdog_page.css('h1[itemprop="name"] .fn').text
     fighter_hash[:nickname] = sherdog_page.css('h1[itemprop="name"] .nickname').text.gsub(/"/, '')
@@ -38,14 +44,12 @@ class MMAService
     fighter_hash[:weight_class] = sherdog_page.css('.item.wclass strong').text
 
     record = sherdog_page.css('.record .count_history')
-
     wins = record.css('.left_side .bio_graph')[0]
     fighter_hash[:wins][:total] = wins.css('.counter').text
     fighter_hash[:wins][:knockouts] = wins.css('.graph_tag:nth-child(3)').text.to_i.to_s
     fighter_hash[:wins][:submissions] = wins.css('.graph_tag:nth-child(5)').text.to_i.to_s
     fighter_hash[:wins][:decisions] = wins.css('.graph_tag:nth-child(7)').text.to_i.to_s
     fighter_hash[:wins][:others] = wins.css('.graph_tag:nth-child(9)').text.to_i.to_s
-
     losses = record.css('.left_side .bio_graph')[1]
     fighter_hash[:losses][:total] = losses.css('.counter').text
     fighter_hash[:losses][:knockouts] = losses.css('.graph_tag:nth-child(3)').text.to_i.to_s
@@ -66,114 +70,89 @@ class MMAService
     return fighter_hash
   end
 
-  # Private class methods below
+  private
 
-  def self.google_query(string)
-    array = string.split(" ")
-    array << "sherdog"
-    array.join("+").to_s
-  end
+    def self.no_profile_found_message
+      "No fighter profile found"
+    end
 
-  def self.google_search(fighter)
-    url = URI.parse(GOOGLE_URL + google_query(fighter))
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    # Nokogiri::HTML(res.body)
-    res.body
-  end
+    def self.build_fights(sherdog_page)
+      fights = []
+      sherdog_page.css('.module.fight_history tr:not(.table_head)').each do |row|
+        fight = fight_template
 
-  def self.sherdog_page(fighter)
-    if sherdog_link(fighter) != "Google search returned no Sherdog links."
-      url = URI.parse(sherdog_link(fighter))
-      req = Net::HTTP::Get.new(url.to_s)
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.request(req)
+        fight[:event] = row.css('td:nth-child(3) a').text
+        fight[:date] = row.css('td:nth-child(3) .sub_line').text
+        fight[:opponent] = row.css('td:nth-child(2) a').text
+        fight[:result] = row.css('td:nth-child(1) .final_result').text
+
+        method = row.css('td:nth-child(4)').text
+        if method.split[0].to_s.include? "Draw"
+          fight[:method] = "Draw"
+        else
+          fight[:method] = method.split[0]
+        end
+        if method[/\(.*?\)/]
+          fight[:method_details] = method[/\(.*?\)/][1..-2]
+        end
+
+        fight[:round] = row.css('td:nth-child(5)').text
+        fight[:time] = row.css('td:nth-child(6)').text
+        fight[:referee] = row.css('td:nth-child(4) .sub_line').text
+
+        if fight[:event] != ""
+          fights << fight
+        end
+      end
+      fights
+    end
+
+    def self.fighter_template
+      {
+        link: "",
+        img_url: "",
+        name: "",
+        nickname: "",
+        age: "",
+        birthday: "",
+        locality: "",
+        nationality: "",
+        flag_url: "",
+        association: "",
+        height: "",
+        weight: "",
+        weight_class: "",
+        wins: {
+          total: 0,
+          knockouts: 0,
+          submissions: 0,
+          decisions: 0,
+          others: 0
+        },
+        losses: {
+          total: 0,
+          knockouts: 0,
+          submissions: 0,
+          decisions: 0,
+          others: 0
+        },
+        draws: 0,
+        no_contests: 0,
+        fights: []
       }
-      return Nokogiri::HTML(res.body)
-    else
-      return sherdog_link(fighter)
     end
-  end
 
-  def self.build_fights(sherdog_page)
-    fights = []
-    sherdog_page.css('.module.fight_history tr:not(.table_head)').each do |row|
-      fight = fight_template
-
-      fight[:event] = row.css('td:nth-child(3) a').text
-      fight[:date] = row.css('td:nth-child(3) .sub_line').text
-      fight[:opponent] = row.css('td:nth-child(2) a').text
-      fight[:result] = row.css('td:nth-child(1) .final_result').text
-
-      method = row.css('td:nth-child(4)').text
-      if method.split[0].to_s.include? "Draw"
-        fight[:method] = "Draw"
-      else
-        fight[:method] = method.split[0]
-      end
-      if method[/\(.*?\)/]
-        fight[:method_details] = method[/\(.*?\)/][1..-2]
-      end
-
-      fight[:round] = row.css('td:nth-child(5)').text
-      fight[:time] = row.css('td:nth-child(6)').text
-      fight[:referee] = row.css('td:nth-child(4) .sub_line').text
-
-      if fight[:event] != ""
-        fights << fight
-      end
+    def self.fight_template
+      {
+        event: "",
+        date: "",
+        opponent: "",
+        result: "",
+        method: "",
+        method_details: "",
+        round: "",
+        time: "",
+        referee: ""
+      }
     end
-    fights
-  end
-
-  def self.fighter_template
-    {
-      link: "",
-      img_url: "",
-      name: "",
-      nickname: "",
-      age: "",
-      birthday: "",
-      locality: "",
-      nationality: "",
-      flag_url: "",
-      association: "",
-      height: "",
-      weight: "",
-      weight_class: "",
-      wins: {
-        total: 0,
-        knockouts: 0,
-        submissions: 0,
-        decisions: 0,
-        others: 0
-      },
-      losses: {
-        total: 0,
-        knockouts: 0,
-        submissions: 0,
-        decisions: 0,
-        others: 0
-      },
-      draws: 0,
-      no_contests: 0,
-      fights: []
-    }
-  end
-
-  def self.fight_template
-    {
-      event: "",
-      date: "",
-      opponent: "",
-      result: "",
-      method: "",
-      method_details: "",
-      round: "",
-      time: "",
-      referee: ""
-    }
-  end
 end
