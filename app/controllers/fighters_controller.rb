ba2a6fc15a2b720593369b4d7ea8ddc4ccdf3d79
yc355 <yifan.i.chen@gmail.com>
2016-05-14 23:07:54 -0400

    debug

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,7 +1,7 @@
 class FightersController < ApplicationController
   def fighter_search
     begin
-      result = MMAService.fighter_query(params[:fighter_search_query])
+      result = MMAService.sherdog_link(params[:fighter_search_query])
       render json: result, status: result.class == Hash ? 200 : 400
     rescue => error
       render json: error
